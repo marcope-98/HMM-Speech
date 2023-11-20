263f9db91537973b7edadcf2cacd07e5f3952063
Marco Peressutti <marco.eugenio.peressutti@gmail.com>
2023-11-20 12:51:29 +0100

    [internal] Minor refactoring of floating point operations
    > Used C-style casting instead of union to improve performance,
    > Added functionalities such as min and max

--- include/hmm/internal/hmmintrin.hpp
+++ include/hmm/internal/hmmintrin.hpp
@@ -8,54 +8,36 @@ namespace hmm
 {
   struct fops
   {
-  private:
-    union cvt
-    {
-      float         f32;
-      std::uint32_t u32{0u};
-      cvt(){};
-      explicit cvt(const float &value) : f32{value} {}
-    };
+    inline static std::uint32_t f32_to_hex(const float &f32) { return *(std::uint32_t *)&f32; }
+    inline static float         hex_to_f32(const std::uint32_t &hex) { return *(float *)&hex; }
 
-  public:
-    static float f32_to_hex(const float &f32)
+    inline static float abs(const float &value)
     {
-      cvt temp(f32);
-      return temp.u32;
+      std::uint32_t u32 = f32_to_hex(value) & 0x7FFFFFFFU;
+      return hex_to_f32(u32);
     }
 
-    static float hex_to_f32(const std::uint32_t &hex)
-    {
-      cvt temp;
-      temp.u32 = hex;
-      return temp.f32;
-    }
+    inline static float max(const float &a, const float &b) { return a > b ? a : b; }
+    inline static float min(const float &a, const float &b) { return a < b ? a : b; }
 
-    static float abs(const float &value)
-    {
-      cvt temp(value);
-      temp.u32 &= (0x7FFFFFFFU);
-      return temp.f32;
-    }
-
-    static bool cmpgt(const float &a, const float &b)
+    inline static bool cmpgt(const float &a, const float &b)
     {
       const float epsilon = std::numeric_limits<float>::epsilon();
       const float fabs_a  = abs(a);
       const float fabs_b  = abs(b);
-      return (a - b) > ((fabs_a < fabs_b ? fabs_b : fabs_a) * epsilon);
+      return (a - b) > (max(fabs_a, fabs_b) * epsilon);
     }
 
-    static bool cmplt(const float &a, const float &b)
+    inline static bool cmplt(const float &a, const float &b)
     {
       const float epsilon = std::numeric_limits<float>::epsilon();
       const float fabs_a  = abs(a);
       const float fabs_b  = abs(b);
-      return (b - a) > ((fabs_a < fabs_b ? fabs_b : fabs_a) * epsilon);
+      return (b - a) > (max(fabs_a, fabs_b) * epsilon);
     }
-    static bool cmpge(const float &a, const float &b) { return !cmplt(a, b); }
-    static bool cmple(const float &a, const float &b) { return !cmpgt(a, b); }
-    static bool cmpeq(const float &a, const float &b) { return !(cmplt(a, b) || cmpgt(a, b)); }
+    inline static bool cmpge(const float &a, const float &b) { return !cmplt(a, b); }
+    inline static bool cmple(const float &a, const float &b) { return !cmpgt(a, b); }
+    inline static bool cmpeq(const float &a, const float &b) { return !(cmplt(a, b) || cmpgt(a, b)); }
   };
 } // namespace hmm
 
